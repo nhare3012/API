279993faadb5166cf4ae87ac8692882bc8884f0b
nhare3012 <nharetawanda@gmail.com>
2022-02-11 14:35:23 +0200

    redoing

--- app.py
+++ app.py
@@ -1,21 +1,15 @@
 from flask import Flask
 from flask_restful import Api
-from routes import Book, BookList, Review, ReviewList
-from flask_cors import CORS
+from routes import BookList, ReviewList, Book, Review
 
 BASE_URL = '/api/bookreactions'
-
 app = Flask(__name__)
-CORS(app)
 
 api = Api(app)
-api.add_resource(BookList, f'{BASE_URL}Books')
-api.add_resource(Book, f'{BASE_URL}/Books/<book_id>')
-api.add_resource(ReviewList, f'{BASE_URL}/Reviews/<book_id>')
-api.add_resource(Review, f'{BASE_URL}/Reviews')
+api.add_resource(BookList, f'{BASE_URL}/Books')
+api.add_resource(Book, f'{BASE_URL}/Book/<book_id>') 
+api.add_resource(ReviewList, f'{BASE_URL}/Reviews/<book_id>') 
+api.add_resource(Review, f'{BASE_URL}/Review')
 
-
-
-
-if __name__ == '__main__':
+if __name__ == '__main__': 
     app.run(debug=True)
--- models.py
+++ models.py
@@ -1,14 +1,13 @@
 class BookModel:
     def __init__(self, title, author, id=-1, cover=''):
-        self.title = title
-        self.author = author
+        self.title = title 
+        self.author = author 
         self.cover = cover
         self.bookId = id
 
 
-
 class ReviewModel:
-    def __init__(self,  content, bookId, id=-1, ):
+    def __init__(self, content, bookId, id=-1):
         self.content = content
-        self.bookId = bookId
+        self.bookId = bookId 
         self.id = id
